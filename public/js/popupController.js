3a2ceb0665f31ec130dfa1dc7803f19661859909
muller <lukasmullerdeoliveira@gmail.com>
2020-12-02 15:10:04 -0300

    modal is working and equipe 1 page is starting to get responsive

--- public/js/popupController.js
+++ public/js/popupController.js
@@ -1,4 +1,19 @@
-function togglePopup(componente){
-    document.getElementById("popup-1").classList.toggle("active")
-    var matricula = componente.getAttribute("data-matricula");
+/* eslint-disable no-unused-vars */
+function togglePopup(componente) {
+  const name = componente.getAttribute('data-name');
+  const email = componente.getAttribute('data-email');
+  const lattes = componente.getAttribute('data-lattes');
+  const linkedin = componente.getAttribute('data-linkedin');
+
+  const nameDiv = document.getElementById('name');
+  const emailDiv = document.getElementById('email');
+  const lattesDiv = document.getElementById('lattes');
+  const linkedinDiv = document.getElementById('link');
+
+  nameDiv.textContent = name;
+  emailDiv.textContent = email;
+  lattesDiv.textContent = lattes;
+  linkedinDiv.textContent = linkedin;
+
+  document.getElementById('popup-1').classList.toggle('active');
 }
