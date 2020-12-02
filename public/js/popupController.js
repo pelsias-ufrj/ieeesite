994d48b9de748f6c813f471c97aafedfe1dd2b2a
muller <lukasmullerdeoliveira@gmail.com>
2020-12-02 12:49:51 -0300

    changed the text format in equipe 1 and started to work on the modal

--- public/js/popupController.js
+++ public/js/popupController.js
@@ -1,3 +1,4 @@
-function togglePopup(){
+function togglePopup(componente){
     document.getElementById("popup-1").classList.toggle("active")
+    var matricula = componente.getAttribute("data-matricula");
 }
